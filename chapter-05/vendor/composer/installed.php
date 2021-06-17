6c01f9352d770cfd9ca111beada55e2e44fc7d15
Jean Pierre DeffoFotso <defcoq73@yahoo.fr>
2021-06-17 22:54:42 +0200

    stop in page 103

---
+++ chapter-05/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a77692e79c19b89a6f82641336c4f69345ec8529',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a77692e79c19b89a6f82641336c4f69345ec8529',
+    ),
+  ),
+);
