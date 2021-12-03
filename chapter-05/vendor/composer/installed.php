1127396d7fce46709fed1dcd2eefc28b64410868
Gallinocoq <defcoq73@yahoo.fr>
2021-12-03 12:11:34 +0100

    modifies utilities.txt

--- chapter-05/vendor/composer/installed.php
+++ chapter-05/vendor/composer/installed.php
@@ -1,24 +1,23 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-main',
-    'version' => 'dev-main',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5f5230ac3173fe2fd0c898f9511d49dee631485f',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => 'a77692e79c19b89a6f82641336c4f69345ec8529',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'a77692e79c19b89a6f82641336c4f69345ec8529',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5f5230ac3173fe2fd0c898f9511d49dee631485f',
+            'dev_requirement' => false,
+        ),
     ),
-  ),
 );
